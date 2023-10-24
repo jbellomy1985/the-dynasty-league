df374db6759f86b2a3b209f8f02b9d1224a7bfb9
Jason Bellomy <jbellomy1985@gmail.com>
2023-10-23 23:13:21 -0500

    Adding Under Construction
    * Adding text to show it is Under Construction

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Under Construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
